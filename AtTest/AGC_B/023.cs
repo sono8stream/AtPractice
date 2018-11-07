5d0527944e05a8fce579d7925a11d58514487d9f
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-11-07 21:49:41 +0900

    Some problems

--- AtTest/AGC_B/023.cs
+++ AtTest/AGC_B/023.cs
@@ -6,7 +6,7 @@ namespace AtTest.AGC_B
 {
     class _023
     {
-        static void Main(string[] args)
+        static void ain(string[] args)
         {
             Method(args);
             Console.ReadLine();
