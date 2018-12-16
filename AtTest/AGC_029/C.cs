5a084a0e06a8b3f731e54a51d8f8e94e7e17d01c
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-12-16 12:58:20 +0900

    AGC_029

---
+++ AtTest/AGC_029/C.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtTest.AGC_029
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            Method(args);
+            Console.ReadLine();
+        }
+
+        static void Method(string[] args)
+        {
+            int n = ReadInt();
+            int[] array = ReadInts();
+            var dict = new Dictionary<int, int>();
+            var lenDict = new Dictionary<int, int>();
+            for(int i = 0; i < n; i++)
+            {
+                if (dict.ContainsKey(array[i]))
+                {
+                    if (!lenDict.ContainsKey(array[i]))
+                    {
+                        lenDict.Add(array[i], 0);
+                    }
+                    lenDict[array[i]] 
+                        = Math.Max(lenDict[array[i]], i - dict[array[i]]);
+                    dict[array[i]] = i;
+                }
+                else
+                {
+                    dict.Add(array[i], i);
+                }
+            }
+        }
+
+        private static string Read() { return Console.ReadLine(); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtTest/D_Challenge/ABC_017.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtTest.D_Challenge
+{
+    class ABC_017
+    {
+        static void ain(string[] args)
+        {
+            Method(args);
+            Console.ReadLine();
+        }
+
+        static void Method(string[] args)
+        {
+            int[] nm = ReadInts();
+            int n = nm[0];
+            int m = nm[1];
+            var fs = new int[n+1];
+            for(int i = 0; i < n; i++)
+            {
+                fs[i] = ReadInt();
+            }
+            fs[n] = fs[n - 1];
+            var sums = new long[n+1];
+            sums[n] = 1;
+            sums[n - 1] = 2;
+            long res = 1;
+            long mask = 1000000000 + 7;
+            var dict = new Dictionary<int, bool>();
+            dict.Add(fs[n - 1], true);
+            for (int i = n - 2; i >= 0; i--)
+            {
+                long delta = 0;
+                if (dict.ContainsKey(fs[i]))
+                {
+                    dict = new Dictionary<int, bool>();
+                    dict.Add(fs[i], true);
+                    delta = sums[i + 1];
+                }
+                else
+                {
+                    dict.Add(fs[i], true);
+                    delta = sums[i + 1] + sums[i + 2];
+                }
+                res += delta;
+                sums[i] = sums[i + 1] + delta;
+                Console.WriteLine(delta + " " + sums[i]);
+            }
+            Console.WriteLine(res);
+        }
+
+        private static string Read() { return Console.ReadLine(); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
