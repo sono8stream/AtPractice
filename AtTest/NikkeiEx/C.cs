abca73758fddb739afde33d56233a62eac51a2e4
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2019-02-18 20:15:04 +0900

    Solve some problems

---
+++ AtTest/NikkeiEx/C.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtTest.NikkeiEx
+{
+    class C
+    {
+        static void ain(string[] args)
+        {
+            Method(args);
+            Console.ReadLine();
+        }
+
+        static void Method(string[] args)
+        {
+            int n = ReadInt();
+            int val = 0;
+            bool plus = true;
+            while (n > 0)
+            {
+                if (plus)
+                {
+                    val += n % 10;
+                }
+                else
+                {
+                    val -= n % 10;
+                }
+                plus = !plus;
+                n /= 10;
+            }
+            while (val < 0) val += 11;
+            Console.WriteLine(val % 11);
+        }
+
+        private static string Read() { return Console.ReadLine(); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtTest/NikkeiEx/E.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtTest.NikkeiEx
+{
+    class E
+    {
+        static void ain(string[] args)
+        {
+            Method(args);
+            Console.ReadLine();
+        }
+
+        static void Method(string[] args)
+        {
+            int n = ReadInt();
+            for (int i = 1; i <= n; i++)
+            {
+                string s = "";
+                bool ok = false;
+                for (int j = 2; j <= 6; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        s += (char)('a' + j - 2);
+                        ok = true;
+                    }
+                }
+                if (!ok) s = i.ToString();
+                Console.WriteLine(s);
+            }
+        }
+
+        private static string Read() { return Console.ReadLine(); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
