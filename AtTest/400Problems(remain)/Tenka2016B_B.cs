b29ea24564c002f62a6993ea7e5a43e9679165ba
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2019-02-19 23:41:43 +0900

    Solve some problems but shallow understanding of Tenka 2016 qual B_B

--- AtTest/400Problems(remain)/Tenka2016B_B.cs
+++ AtTest/400Problems(remain)/Tenka2016B_B.cs
@@ -6,7 +6,7 @@ namespace AtTest._400Problems_remain_
 {
     class Tenka2016B_B
     {
-        static void ain(string[] args)
+        static void Main(string[] args)
         {
             Method(args);
             Console.ReadLine();
@@ -15,16 +15,59 @@ namespace AtTest._400Problems_remain_
         static void Method(string[] args)
         {
             string s = Read();
-            var dp = new int[101, 101, 101];
-            for (int i = 0; i <= 100; i++)
-                for (int j = 0; j <= 100; j++)
-                    for (int k = 0; k <= 100; k++)
-                        dp[i, j, k] = int.MaxValue;
-            dp[0, 0, 0] = 0;
-            for(int i = 0; i < s.Length; i++)
+            int length = s.Length;
+            var dp = new int[105, 105,105];
+            int inf = int.MaxValue / 2;
+            //iマスまで見てカーソル位置がj，(の数-)の数がkの操作数
+            for (int i = 0; i < 101; i++)
             {
-                //for(int j=0;j<)
+                for(int j = 0; j < 101; j++)
+                {
+                    for (int k = 0; k < 101; k++)
+                    {
+                        dp[i, j, k] = inf;
+                    }
+                }
             }
+            dp[0, 0, 0] = 0;
+
+            for(int i = 0; i < length; i++)
+            {
+                for(int j = 0; j < length; j++)
+                {
+                    for(int k = 0; k <= length; k++)
+                    {
+                        if (dp[i, j, k] == int.MaxValue) continue;
+
+                        if (s[i] == '(')
+                        {
+                            dp[i + 1, j,k + 1]
+                                = Math.Min(dp[i + 1, j, k + 1], dp[i, j, k]);
+                            if (0 < k)
+                            {
+                                dp[i + 1, i, k - 1]
+                                    = Math.Min(dp[i + 1, i, k - 1], dp[i, j, k] + 1);
+                            }
+                        }
+                        else
+                        {
+                            dp[i + 1, i, k + 1]
+                                = Math.Min(dp[i + 1, i, k + 1], dp[i, j, k] + 1);
+                            if (0 < k)
+                            {
+                                dp[i + 1, j, k - 1]
+                                    = Math.Min(dp[i + 1, j, k - 1], dp[i, j, k]);
+                            }
+                        }
+                    }
+                }
+            }
+            int res = inf;
+            for(int i = 0; i < length; i++)
+            {
+                res = Math.Min(res, dp[length, i, 0] + i);
+            }
+            Console.WriteLine(res);
         }
 
         private static string Read() { return Console.ReadLine(); }
