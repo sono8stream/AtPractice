70c84ed1c44c059dc6f45e5af49768ea4a7ceb09
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2019-02-22 23:00:18 +0900

    Solve some problems

---
+++ AtTest/AGC_Challenge/013_B.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtTest.AGC_Challenge
+{
+    class _013_B
+    {
+        static void ain(string[] args)
+        {
+            Method(args);
+            Console.ReadLine();
+        }
+
+        static void Method(string[] args)
+        {
+            int[] nm = ReadInts();
+            int n = nm[0];
+            int m = nm[1];
+            List<int>[] graph = new List<int>[n];
+            for(int i = 0; i < n; i++)
+            {
+                graph[i] = new List<int>();
+            }
+            for(int i = 0; i < m; i++)
+            {
+                int[] ab = ReadInts();
+                int a = ab[0] - 1;
+                int b = ab[1] - 1;
+                graph[a].Add(b);
+                graph[b].Add(a);
+            }
+        }
+
+        private static string Read() { return Console.ReadLine(); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
--- AtTest/TDPC/G.cs
+++ AtTest/TDPC/G.cs
@@ -14,21 +14,62 @@ namespace AtTest.TDPC
 
         static void Method(string[] args)
         {
-            string[] input = Console.ReadLine().Split(' ');
-            int n = int.Parse(input[0]);
-            for (int i = 0; i < n; i++)
+            string s = Read();
+            long k = ReadLong();
+            long[,] dp = new long[s.Length, 26];
+            dp[s.Length - 1, s[s.Length - 1] - 'a'] = 1;
+            long prevAll = 1;
+            long limit = long.MaxValue / 2;
+            for (int i = s.Length - 2; i >= 0; i--)
             {
-
+                long nowAll = 0;
+                for(int j = 0; j < 26; j++)
+                {
+                    if (s[i] - 'a' == j)
+                    {
+                        dp[i, j] = prevAll + 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i + 1, j];
+                    }
+                    nowAll = Math.Min(nowAll + dp[i, j], limit);
+                }
+                prevAll = nowAll;
             }
-            Console.WriteLine("text");
+            if (prevAll < k)
+            {
+                Console.WriteLine("Eel");
+                return;
+            }
+            for(int i = 0; i < s.Length && 0 < k; i++)
+            {
+                int c = 0;
+                for(int j = 0; j < 26; j++)
+                {
+                    if (k - dp[i, j] > 0)
+                    {
+                        k -= dp[i, j];
+                    }
+                    else
+                    {
+                        k--;
+                        c = j;
+                        break;
+                    }
+                }
+                while (s[i] - 'a' != c) i++;
+                Console.Write(s[i]);
+            }
+            Console.WriteLine();//これ大事！
         }
 
         private static string Read() { return Console.ReadLine(); }
         private static int ReadInt() { return int.Parse(Read()); }
         private static long ReadLong() { return long.Parse(Read()); }
-        private static double ReadDouble() { return double.Parse(Read()); }
+        private static long ReadDouble() { return long.Parse(Read()); }
         private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
         private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
-        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+        private static long[] ReadDoubles() { return Array.ConvertAll(Read().Split(), long.Parse); }
     }
 }
