6586e7e354e0b912d72d2a0329ad26ecce36c96d
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2019-09-06 01:24:00 +0900

    Solve some problems

---
+++ AtTest/700problems/ARC069_E.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest._700problems
+{
+    class ARC069_E
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int n = ReadInt();
+            long[] array = ReadLongs();
+            long[] res = new long[n];
+            PriorityQueue<long> queue = new PriorityQueue<long>();
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(array[i], 1);
+            }
+            long tmpMax = 0;
+            for (int i =0;i<n;i++)
+            {
+                if (tmpMax >= array[i]) continue;
+
+                while (queue.Count > 0 && array[i] >= queue.Top().Key)
+                {
+                    var pair = queue.Dequeue();
+                    res[i] += pair.Key - tmpMax;
+                }
+                res[i] += (array[i] - tmpMax) * queue.Count;
+                tmpMax = array[i];
+            }
+            for (int i = 0; i < n; i++) WriteLine(res[i]);
+        }
+
+        class PriorityQueue<T>
+        {
+            private readonly List<KeyValuePair<long, T>> list;
+
+            public int Count { get; private set; }
+
+            public PriorityQueue()
+            {
+                list = new List<KeyValuePair<long, T>>();
+                Count = 0;
+            }
+
+            private void Add(KeyValuePair<long, T> pair)
+            {
+                if (Count == list.Count)
+                {
+                    list.Add(pair);
+                }
+                else
+                {
+                    list[Count] = pair;
+                }
+                Count++;
+            }
+
+            private void Swap(int a, int b)
+            {
+                KeyValuePair<long, T> tmp = list[a];
+                list[a] = list[b];
+                list[b] = tmp;
+            }
+
+            public void Enqueue(long key, T value)
+            {
+                Add(new KeyValuePair<long, T>(key, value));
+                int c = Count - 1;
+                while (c > 0)
+                {
+                    int p = (c - 1) / 2;
+                    if (list[c].Key >= list[p].Key) break;
+
+                    Swap(p, c);
+                    c = p;
+                }
+            }
+
+            public KeyValuePair<long, T> Top()
+            {
+                return list[0];
+            }
+
+            public KeyValuePair<long, T> Dequeue()
+            {
+                KeyValuePair<long, T> pair = list[0];
+                Count--;
+                if (Count == 0) return pair;
+
+                list[0] = list[Count];
+                int p = 0;
+                while (true)
+                {
+                    int c1 = p * 2 + 1;
+                    int c2 = p * 2 + 2;
+                    if (c1 >= Count) break;
+
+                    int c = (c2 >= Count || list[c1].Key < list[c2].Key)
+                        ? c1 : c2;
+                    if (list[c].Key >= list[p].Key) break;
+
+                    Swap(p, c);
+                    p = c;
+                }
+                return pair;
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtTest/NothingToLearn/ABC082_B.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.NothingToLearn
+{
+    class ABC082_B
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            string s = Read();
+            string t = Read();
+            char[] ss = s.ToCharArray();
+            char[] tt = t.ToCharArray();
+            Array.Sort(ss);
+            Array.Sort(tt);
+            Array.Reverse(tt);
+            s = new string(ss);
+            t = new string(tt);
+            if (s.CompareTo(t) == -1)
+            {
+                WriteLine("Yes");
+            }
+            else
+            {
+                WriteLine("No");
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
