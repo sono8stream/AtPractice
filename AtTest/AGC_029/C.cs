e3ac090d21bcc0b3c61040af80f4f8188877dd5a
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2020-02-09 16:25:21 +0900

    Solve Difficult problems

--- AtTest/AGC_029/C.cs
+++ AtTest/AGC_029/C.cs
@@ -16,29 +16,83 @@ namespace AtTest.AGC_029
         {
             int n = ReadInt();
             int[] array = ReadInts();
-            long bottom=0;
+            long bottom = 0;
             long top = int.MaxValue;
             while (bottom + 1 < top)
             {
                 long mid = (bottom + top) / 2;
-                var que = new Queue<int[]>();
-                int len = 0;
-                for(int i = 0; i < n; i++)
+                var stack = new Stack<long[]>();
+                bool can = true;
+                stack.Push(new long[2] { 1, array[0] });
+                for(int i = 1; i < n; i++)
                 {
-                    if (len < array[i])
+                    if (array[i - 1] < array[i])
                     {
-                        que.Enqueue(new int[2] { 1, array[i] - len });
-                        len = array[i];
+                        stack.Push(new long[2] { 1, array[i] - array[i - 1] });
                     }
                     else
                     {
-                        while (array[i] < len)
+                        long sum = 0;
+                        while (stack.Count > 0 
+                            && sum + stack.Peek()[1] <= array[i - 1] - array[i])
                         {
+                            sum += stack.Pop()[1];
+                        }
+                        stack.Peek()[1] -= array[i - 1] - array[i] - sum;
 
+                        if (stack.Peek()[0] == mid)
+                        {
+                            if (stack.Count == 1)
+                            {
+                                can = false;
+                                break;
+                            }
+                            long cnt = stack.Peek()[1];
+                            stack.Pop();
+                            Add(stack);
+                            stack.Push(new long[2] { 1, cnt });
+                        }
+                        else
+                        {
+                            Add(stack);
                         }
                     }
+                    Normalize(stack);
+                }
+                if (can) top = mid;
+                else bottom = mid;
+            }
+            Console.WriteLine(top);
+        }
+
+        static void Normalize(Stack<long[]> stack)
+        {
+            if (stack.Count > 1)
+            {
+                long[] peek = stack.Pop();
+                if (stack.Peek()[0] == peek[0])
+                {
+                    stack.Peek()[1] += peek[1];
+                }
+                else
+                {
+                    stack.Push(peek);
                 }
             }
+        }
+
+        static void Add(Stack<long[]> stack)
+        {
+            if (stack.Peek()[1] == 1)
+            {
+                stack.Peek()[0]++;
+            }
+            else
+            {
+                stack.Peek()[1]--;
+                stack.Push(new long[2] { stack.Peek()[0] + 1, 1 });
+            }
+            Normalize(stack);
         }
 
         private static string Read() { return Console.ReadLine(); }
---
+++ AtTest/ARC_C/041.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.ARC_C
+{
+    class _041
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int[] nl = ReadInts();
+            int n = nl[0];
+            int l = nl[1];
+            long[][] xds = new long[n][];
+            for(int i = 0; i < n; i++)
+            {
+                string[] xd = Read().Split();
+                xds[i] = new long[2] { int.Parse(xd[0]), xd[1][0] };
+            }
+            long first = xds[0][1];
+            List<long[]> blocks = new List<long[]>();
+            long now = first;
+            blocks.Add(new long[2] { 1, xds[0][0] });
+            for(int i = 1; i < n; i++)
+            {
+                if (now == xds[i][1])
+                {
+                    blocks[blocks.Count - 1][0]++;
+                    if (now == 'R')
+                    {
+                        blocks[blocks.Count - 1][1] = xds[i][0];
+                    }
+                }
+                else
+                {
+                    blocks.Add(new long[2] { 1, xds[i][0] });
+                    now = xds[i][1];
+                }
+            }
+            long res = 0;
+            now = first;
+            int tmp = 0;
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (now != xds[i][1])
+                {
+                    now = xds[i][1];
+                    tmp++;
+                    cnt = 0;
+                }
+
+                if (now == 'L')
+                {
+                    res += xds[i][0] - blocks[tmp][1] - cnt;
+                }
+                else
+                {
+                    res += blocks[tmp][1] - xds[i][0] - (blocks[tmp][0] - cnt - 1);
+                }
+                cnt++;
+            }
+            if (first == 'L')
+            {
+                res += (blocks[0][1] - 1) * blocks[0][0];
+                blocks.RemoveAt(0);
+            }
+            for (int i = 0; i + 1 < blocks.Count; i += 2)
+            {
+                long more = Max(blocks[i][0], blocks[i + 1][0]);
+                long delta = blocks[i + 1][1] - blocks[i][1] - 1;
+                res += delta * more;
+            }
+            if (blocks.Count % 2 == 1)
+            {
+                res += (l - blocks[blocks.Count - 1][1]) * blocks[blocks.Count - 1][0];
+            }
+            WriteLine(res);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
