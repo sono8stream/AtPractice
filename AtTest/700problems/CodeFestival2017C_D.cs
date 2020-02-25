b0b99b5ee69da5f44b90dd5d5e8b6ad206862a48
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2020-02-25 20:08:12 +0900

    ABC 156

---
+++ AtTest/700problems/CodeFestival2017C_D.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest._700problems
+{
+    class CodeFestival2017C_D
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            string s = Read();
+
+            var dict = new Dictionary<string, List<int>>();
+            dict.Add("", new List<int>());
+            dict[""].Add(0);
+            bool[] now = new bool[26];
+            string[] codes = new string[s.Length + 1];
+            codes[0] = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                now[s[i] - 'a'] = !now[s[i] - 'a'];
+                codes[i + 1] = "";
+                for (int j = 0; j < 26; j++)
+                {
+                    if (now[j])
+                    {
+                        codes[i+1] += (char)('a' + j);
+                        if (i % 2 == 0 && s[i] == 'a' + j)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                if (!dict.ContainsKey(codes[i+1]))
+                {
+                    dict.Add(codes[i+1], new List<int>());
+                }
+                dict[codes[i+1]].Add(i + 1);
+
+                    if (!dict.ContainsKey(codes[i]))
+                    {
+                        dict.Add(codes[i], new List<int>());
+                    }
+                dict[codes[i]].Add(i + 1);
+                 }
+               
+            int[] dp = new int[s.Length + 1];
+            for(int i = 0; i <= s.Length; i++)
+            {
+                dp[i] = int.MaxValue / 2;
+            }
+            dp[0] = 0;
+
+            now = new bool[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                string code = "";
+                for (int j = 0; j < 26; j++)
+                {
+                    if (now[j])
+                    {
+                        code += (char)('a' + j);
+                    }
+                }
+                dp[i + 1] = Min(dp[i + 1], dp[i]);
+                if (i==dict[code][0])
+                {
+                    for(int j = 0; j < dict[code].Count; j++)
+                    {
+                        int to = dict[code][j];
+                        dp[to] = Min(dp[to], dp[i]);
+                    }
+                }
+                now[s[i] - 'a'] = !now[s[i] - 'a'];
+            }
+            WriteLine(dp[s.Length]);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
--- AtTest/ABC_155/F.cs
+++ AtTest/ABC_155/F.cs
@@ -21,6 +21,133 @@ namespace AtTest.ABC_155
 
         static void Method(string[] args)
         {
+            int[] nm = ReadInts();
+            int n = nm[0];
+            int m = nm[1];
+            int[][] abs = new int[n + 1][];
+            abs[0] = new int[2] { 0, 0 };
+            for (int i = 0; i < n; i++)
+            {
+                abs[i+1] = ReadInts();
+            }
+            Array.Sort(abs, (a, b) => a[0] - b[0]);
+            int[][] lrs = new int[m][];
+            for(int i = 0; i < m; i++)
+            {
+                lrs[i] = ReadInts();
+            }
+            UnionFind unionFind = new UnionFind(n);
+            for(int i = 0; i < m; i++)
+            {
+                if (lrs[i][1] < abs[0][0]
+                    || lrs[i][0] > abs[n][0])
+                {
+                    continue;
+                }
+                int lOK = 1;
+                int lNG = n + 1;
+                while (Abs(lOK - lNG) > 1)
+                {
+                    int mid = (lOK + lNG) / 2;
+                    if(lrs[i][0]<= abs[mid][0])
+                    {
+                        lOK = mid;
+                    }
+                    else
+                    {
+                        lNG = mid;
+                    }
+                }
+                int rOK = n;
+                int rNG = 0;
+                while (Abs(rOK - rNG) > 1)
+                {
+                    int mid = (rOK + rNG / 2);
+                    if (lrs[i][1] >= abs[mid][0])
+                    {
+                        rOK = mid;
+                    }
+                    else
+                    {
+                        rNG = mid;
+                    }
+                }
+                if (lOK == rOK)
+                {
+                    continue;
+                }
+
+                if(abs[lOK][1]!=abs[lOK-1][1]
+                    && abs[rOK - 1][1]!=abs[rOK][1])
+                {
+                    unionFind.Unite(lOK, rOK);
+                }
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                if (Abs(abs[i][1] - abs[i + 1][1]) == unionFind.GetSize(i) % 2)
+                {
+                    WriteLine(-1);
+                    return;
+                }
+            }
+
+
+        }
+
+        class UnionFind
+        {
+            int[] tree;
+            int[] size;
+
+            public UnionFind(int length)
+            {
+                tree = new int[length];
+                size = new int[length];
+                for (int i = 0; i < length; i++)
+                {
+                    tree[i] = i;
+                    size[i] = 1;
+                }
+            }
+
+            public int Root(int x)
+            {
+                int rx = x;
+                while (tree[rx] != rx)
+                {
+                    rx = tree[rx];
+                }
+                tree[x] = rx;
+                return rx;
+            }
+
+            public bool IsSame(int x, int y)
+            {
+                return Root(x) == Root(y);
+            }
+
+            public void Unite(int x, int y)
+            {
+                int rx = Root(x);
+                int ry = Root(y);
+                if (rx == ry) return;
+
+                if (rx > ry)
+                {
+                    int temp = rx;
+                    rx = ry;
+                    ry = temp;
+                }
+                tree[ry] = rx;
+                size[rx] += size[ry];
+            }
+
+            public int GetSize(int x)
+            {
+                return size[Root(x)];
+            }
         }
 
         private static string Read() { return ReadLine(); }
