8826037636a476fab97c2b3a10ebd68fb7ccee9d
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2020-02-28 17:18:51 +0900

    Solve yellow difficulty

---
+++ AtTest/AGC_Challenge/008_D.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.AGC_Challenge
+{
+    class _008_D
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int n = ReadInt();
+            int[] xs = ReadInts();
+            int[][] xIndexes = new int[n][];
+            for(int i = 0; i < n; i++)
+            {
+                xIndexes[i] = new int[2] { xs[i] - 1, i + 1 };
+            }
+            Array.Sort(xIndexes, (a, b) => a[0] - b[0]);
+            Queue<int> que = new Queue<int>();
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 1; j < xIndexes[i][1]; j++)
+                {
+                    que.Enqueue(xIndexes[i][1]);
+                }
+            }
+            int[] res = new int[n * n];
+            int now = 0;
+            int[] cnts = new int[n];
+            for(int i = 0; i < n * n; i++)
+            {
+                if (now < n && i == xIndexes[now][0])
+                {
+                    if (cnts[xIndexes[now][1] - 1] == xIndexes[now][1] - 1)
+                    {
+                        res[i] = xIndexes[now][1];
+                        cnts[xIndexes[now][1] - 1]++;
+                        for (int j = xIndexes[now][1]; j < n; j++)
+                        {
+                            que.Enqueue(xIndexes[now][1]);
+                        }
+                        now++;
+                    }
+                    else
+                    {
+                        WriteLine("No");
+                        return;
+                    }
+                }
+                else
+                {
+                    if (que.Count > 0)
+                    {
+                        res[i] = que.Peek();
+                        cnts[que.Dequeue() - 1]++;
+                    }
+                    else
+                    {
+                        WriteLine("No");
+                        return;
+                    }
+                }
+            }
+
+            WriteLine("Yes");
+            for(int i = 0; i < n*n; i++)
+            {
+                Write(res[i]+" ");
+            }
+            WriteLine();
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtTest/ARC_C/099.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.ARC_C
+{
+    class _099
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int[] nm = ReadInts();
+            int n = nm[0];
+            int m = nm[1];
+            HashSet<int>[] graph = new HashSet<int>[n];
+            for(int i = 0; i < n; i++)
+            {
+                graph[i] = new HashSet<int>();
+                for(int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    graph[i].Add(j);
+                }
+            }
+            for(int i = 0; i < m; i++)
+            {
+                int[] ab = ReadInts();
+                int a = ab[0] - 1;
+                int b = ab[1] - 1;
+                graph[a].Remove(b);
+                graph[b].Remove(a);
+            }
+
+            List<int> blocks = new List<int>();
+            int[] states = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                if (states[i]>0)
+                {
+                    continue;
+                }
+
+                Queue<int[]> que = new Queue<int[]>();
+                que.Enqueue(new int[2] { i, 1 });
+                int score = 0;
+                while (que.Count > 0)
+                {
+                    int[] val = que.Dequeue();
+                    int index = val[0];
+                    int state = val[1];
+
+                    if (states[index] > 0)
+                    {
+                        if (states[index] % 2 == state%2)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            WriteLine(-1);
+                            return;
+                        }
+                    }
+
+                    states[index] = state;
+                    score += state % 2 == 0 ? 1 : -1;
+                    foreach(int to in graph[index])
+                    {
+                        que.Enqueue(new int[2] { to,state + 1 });
+                    }
+                }
+                blocks.Add(score);
+            }
+            HashSet<int> vals = new HashSet<int>();
+            vals.Add(0);
+            for(int i = 0; i < blocks.Count; i++)
+            {
+                HashSet<int> next = new HashSet<int>();
+                foreach(int val in vals)
+                {
+                    next.Add(val + blocks[i]);
+                    next.Add(val - blocks[i]);
+                }
+                vals = next;
+            }
+            int min = int.MaxValue;
+            foreach (int val in vals)
+            {
+                int a = (n + val) / 2;
+                int b = (n - val) / 2;
+                int tmp = (a * (a - 1) + b * (b - 1)) / 2;
+                min = Min(min, tmp);
+            }
+            WriteLine(min);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
