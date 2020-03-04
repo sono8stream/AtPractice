e4626a90005a5a90e9b3b8c08d666b9c85272222
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2020-03-04 23:46:39 +0900

    ABC 156 D

---
+++ AtTest/ABC_156/D.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.ABC_156
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            long[] nab = ReadLongs();
+            long n = nab[0];
+            long a = nab[1];
+            long b = nab[2];
+            long mask = 1000000000 + 7;
+
+            long[] pows = new long[40];
+            pows[0] = 2;
+            for (int i = 1; i < pows.Length; i++)
+            {
+                pows[i] = pows[i - 1] * pows[i - 1];
+                pows[i] %= mask;
+            }
+            long all = 1;
+            long tmp = 1;
+            for (int i = 0; i < 40; i++)
+            {
+                if ((n / tmp) % 2 == 1)
+                {
+                    all *= pows[i];
+                    all %= mask;
+                }
+                tmp *= 2;
+            }
+            all = (all - 1 + mask) % mask;
+
+            CaseCalculator calculator = new CaseCalculator(mask, Max(a, b));
+            long nA = 1;
+            long nB = 1;
+            for (long i = n; i >= n - a + 1; i--)
+            {
+                nA *= i;
+                nA %= mask;
+            }
+            for (long i = n; i >= n - b + 1; i--)
+            {
+                nB *= i;
+                nB %= mask;
+            }
+            all = (all - (nA * calculator.Inverse(calculator.Permutation(a, a))) % mask + mask) % mask;
+            all = (all - (nB * calculator.Inverse(calculator.Permutation(b, b))) % mask + mask) % mask;
+            WriteLine(all);
+        }
+
+        class CaseCalculator
+        {
+            long mask;
+            long[] allPermutations;
+            long[] allInverses;
+
+            public CaseCalculator(long mask, long permutationCnt)
+            {
+                this.mask = mask;
+                allPermutations = AllPermutations(permutationCnt);
+                allInverses = AllInverses(permutationCnt);
+            }
+
+            public long Combination(long n, long m)
+            {
+                if (n < m) return 0;
+
+                if (n - m < m) m = n - m;
+
+                return Multi(allPermutations[n], allInverses[n - m], allInverses[m]);
+            }
+
+            public long Permutation(long n, long m)
+            {
+                if (n < m) return 0;
+
+                return Multi(allPermutations[n], allInverses[n - m]);
+            }
+
+            long[] AllPermutations(long n)
+            {
+                var perms = new long[n + 1];
+                perms[0] = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    perms[i] = Multi(perms[i - 1], i);
+                }
+                return perms;
+            }
+
+            long[] AllInverses(long n)
+            {
+                var inverses = new long[n + 1];
+                inverses[1] = 1;
+                var permInverses = new long[n + 1];
+                permInverses[0] = 1;
+                permInverses[1] = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    inverses[i] = mask - inverses[mask % i] * (mask / i) % mask;
+                    permInverses[i] = Multi(permInverses[i - 1], inverses[i]);
+                }
+                return permInverses;
+            }
+
+            public long Multi(params long[] vals)
+            {
+                if (vals.Length == 0)
+                {
+                    return 0;
+                }
+
+                long val = vals[0] % mask;
+                for (int i = 1; i < vals.Length; i++)
+                {
+                    val *= (vals[i] % mask);
+                    val %= mask;
+                }
+                return val;
+            }
+
+            public long Inverse(long val)
+            {
+                long a = mask;
+                long b = val % mask;
+                long x = 1;
+                long x1 = 0;
+                long y = 0;
+                long y1 = 1;
+                while (b > 0)
+                {
+                    long q = a / b;
+                    long r = a % b;
+                    long x2 = (mask + x - q * x1) % mask;
+                    long y2 = (mask + y - q * y1) % mask;
+                    a = b;
+                    b = r;
+                    x = x1;
+                    x1 = x2;
+                    y = y1;
+                    y1 = y2;
+                }
+                return y;
+            }
+
+            long DeprecatedInverse(long val)
+            {
+                return Pow(val, mask - 2);
+            }
+
+            long Pow(long a, long exp)
+            {
+                if (exp == 0) return 1;
+                else if (exp == 1) return a % mask;
+                else
+                {
+                    long halfMod = Pow(a, exp / 2);
+                    long nextMod = Multi(halfMod, halfMod);
+                    if (exp % 2 == 0)
+                    {
+                        return nextMod;
+                    }
+                    else
+                    {
+                        return Multi(nextMod, a);
+                    }
+                }
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtTest/ABC_157/F.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.ABC_157
+{
+    class F
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int[] nk = ReadInts();
+            int n = nk[0];
+            int k = nk[1];
+            double[][] xycs = new double[n][];
+            for(int i = 0; i < n; i++)
+            {
+                xycs[i] = ReadDoubles();
+            }
+            double bottom = 0;
+            double top = 10000;
+            double thres = 0.0000001;
+            while (bottom + thres < top)
+            {
+                double mid = (bottom + top) / 2;
+
+                List<double[]> poses = new List<double[]>();
+                for(int i = 0; i < n; i++)
+                {
+                    double x1 = xycs[i][0];
+                    double y1 = xycs[i][1];
+                    double r1 = mid / xycs[i][2];
+                    poses.Add(new double[2] { xycs[i][0], xycs[i][1] });
+                    for(int j = i + 1; j < n; j++)
+                    {
+                        double x2 = xycs[j][0];
+                        double y2 = xycs[j][1];
+                        double r2 = mid / xycs[j][2];
+                        double dist = Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+                        if (dist > Abs(r1 - r2) && dist < r1 + r2)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
