bc0c4047d56d9ef100cff4aded0d6ac2503655fb
SONO <son.stream8580@gmail.com>
2020-09-23 21:58:12 +0900

    Remove Main function

--- AtTest/Library/SegmentTree/LazySegmentTree.cs
+++ AtTest/Library/SegmentTree/LazySegmentTree.cs
@@ -9,7 +9,7 @@ namespace AtTest.Library.SegmentTree
 {
     class ABC179_F
     {
-        static void Main(string[] args)
+        static void ain(string[] args)
         {
             var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
             SetOut(sw);
--- AtTest/Library/SegmentTree/SegmentTree.cs
+++ AtTest/Library/SegmentTree/SegmentTree.cs
@@ -9,7 +9,7 @@ namespace AtTest.Library.SegmentTree
 {
     class ARC033_C
     {
-        static void Main(string[] args)
+        static void ain(string[] args)
         {
             var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
             SetOut(sw);
