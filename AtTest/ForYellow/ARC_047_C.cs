79f2c561aa6a085f2bf9f29affdc867cc10f0634
SONO <son.stream8580@gmail.com>
2020-11-14 18:53:42 +0900

    Wanna be yellow

---
+++ AtTest/ForYellow/ARC_047_C.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.ForYellow
+{
+    class ARC_047_C
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int[] nk = ReadInts();
+            int n = nk[0];
+            int k = nk[1];
+
+            var tree = new SegmentTree<int>(n + 1, (a, b) => a + b, 1, 0);
+
+            long remain = 1;
+            bool[] used = new bool[n + 1];
+            for(int i = 0; i <= n-k; i++)
+            {
+                long now = 0;
+                if (i < n-k)
+                {
+                    remain *= n - i;
+                    if (remain % k == 0)
+                    {
+                        now = remain / k;
+                        remain = k;
+                    }
+                    else
+                    {
+                        now = remain / k + 1;
+                        remain %= k;
+                    }
+                }
+                if (i == n - k)
+                {
+                    now = remain;
+                }
+
+                int bottom = 0;
+                int top = n;
+                while (bottom + 1 < top)
+                {
+                    int mid = (bottom + top) / 2;
+                    int val = tree.Scan(1, mid);
+                    if (val >= now)
+                    {
+                        top = mid;
+                    }
+                    else
+                    {
+                        bottom = mid;
+                    }
+                }
+                used[top] = true;
+                WriteLine(top);
+                tree.Update(top, 0);
+            }
+
+            int last = n;
+            for(int i = n - k + 1; i < n; i++)
+            {
+                while (used[last])
+                {
+                    last--;
+                }
+                WriteLine(last);
+                used[last] = true;
+            }
+        }
+
+        class SegmentTree<T>
+        {
+            int totalLength;
+            T[] tree;
+            Func<T, T, T> integrate;
+            T exValue;
+
+            public SegmentTree(int length, Func<T, T, T> integrate, T initialValue, T exValue)
+            {
+                totalLength = 1;
+                while (totalLength < length)
+                {
+                    totalLength *= 2;
+                }
+
+                this.integrate = integrate;
+
+                tree = new T[totalLength * 2 - 1];
+                for (int i = 0; i < tree.Length; i++)
+                {
+                    tree[i] = exValue;
+                }
+                for (int i = 0; i < length; i++)
+                {
+                    tree[i + totalLength - 1] = initialValue;
+                }
+                this.exValue = exValue;
+
+                UpdateAll();
+            }
+
+            void UpdateAll()
+            {
+                for (int i = totalLength - 2; i >= 0; i--)
+                {
+                    tree[i] = integrate(tree[i * 2 + 1], tree[i * 2 + 2]);
+                }
+            }
+
+            public void Update(int i, T value)
+            {
+                int now = i + totalLength - 1;
+                tree[now] = value;
+                while (now > 0)
+                {
+                    now = (now - 1) / 2;
+                    tree[now] = integrate(tree[now * 2 + 1], tree[now * 2 + 2]);
+                }
+            }
+
+
+            /// <summary>
+            /// [left, right]を取得
+            /// </summary>
+            /// <param name="left"></param>
+            /// <param name="right"></param>
+            public T Scan(int left, int right)
+            {
+                if (left >= totalLength || right < 0 || right < left)
+                {
+                    return exValue;
+                }
+
+                return Query(left, right, 0, 0, totalLength - 1);
+            }
+
+            /// <summary>
+            /// [left, right]が[top,last]と一致していればその値を，そうでなければ統合して返す
+            /// 再帰処理
+            /// </summary>
+            /// <param name="left"></param>
+            /// <param name="right"></param>
+            /// <param name="i"></param>
+            /// <param name="top"></param>
+            /// <param name="last"></param>
+            T Query(int left, int right, int i, int top, int last)
+            {
+                if (left == top && right == last)
+                {
+                    return tree[i];
+                }
+                else
+                {
+                    int half = (top + last) / 2;
+                    T val = exValue;
+                    if (left <= half)
+                    {
+                        val = integrate(val, Query(left, Min(right, half), i * 2 + 1, top, half));
+                    }
+                    if (right >= half + 1)
+                    {
+                        val = integrate(val, Query(Max(left, half + 1), right, i * 2 + 2, half + 1, last));
+                    }
+                    return val;
+                }
+            }
+
+            public T Look(int i)
+            {
+                return tree[i + totalLength - 1];
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
--- AtTest/M_Solutions_2020/E.cs
+++ AtTest/M_Solutions_2020/E.cs
@@ -22,39 +22,193 @@ namespace AtTest.M_Solutions_2020
         static void Method(string[] args)
         {
             int n = ReadInt();
-            int[][] xyps = new int[n][];
+            long[][] xyps = new long[n][];
             for(int i = 0; i < n; i++)
             {
-                xyps[i] = ReadInts();
+                xyps[i] = ReadLongs();
             }
 
             int all = 1 << n;
-            for (int k = 0; k <= n; k++) {
-                int res = int.MaxValue;
 
-                for (int i = 0; i < all; i++)
+            long[] horizonBars=new long[all];
+            for(int i = 0; i < all; i++)
+            {
+                List<int> xs = new List<int>();
+                for (int j = 0; j < n; j++)
                 {
-                    List<int> horizons = new List<int>();
-                    List<int> verticals = new List<int>();
-                    int div = 1;
-                    for (int j = 0; j < n; j++)
+                    if ((i & (1 << j)) > 0)
                     {
-                        if ((div & i) > 0)
-                        {
-                            horizons.Add(xyps[i][1]);
-                        }
-                        else
-                        {
-                            verticals.Add(xyps[i][0]);
-                        }
-                    }
-
-                    for(int j = 0; j <= k; j++)
-                    {
-                        int other = k - j;
-
+                        xs.Add(j);
                     }
                 }
+                int[] array = xs.ToArray();
+                Array.Sort(array, (a, b) => (int)(xyps[a][0] - xyps[b][0]));
+
+                long min = long.MaxValue;
+                for(int j = 0; j < array.Length; j++)
+                {
+                    long tmp = 0;
+                    for(int k = 0; k < array.Length; k++)
+                    {
+                        tmp += xyps[array[k]][2] * Abs(xyps[array[j]][0] - xyps[array[k]][0]);
+                    }
+                    min = Min(min, tmp);
+                }
+
+                horizonBars[i] = min;
+            }
+
+            long[,] horizons = new long[all, n + 1];
+            for(int i = 1; i < all; i++)
+            {
+                List<int> xs = new List<int>();
+                for(int j = 0; j < n; j++)
+                {
+                    if ((i & (1 << j)) > 0)
+                    {
+                        xs.Add(j);
+                    }
+                }
+                int[] array = xs.ToArray();
+                Array.Sort(array, (a, b) => (int)(xyps[a][0] - xyps[b][0]));
+
+                long[,] dp = new long[array.Length, n + 1];
+                for(int j = 0; j < array.Length; j++)
+                {
+                    long x = xyps[array[j]][0];
+                    dp[j, 0] = Abs(x) * xyps[array[j]][2];
+                    if (j > 0)
+                    {
+                        dp[j, 0] += dp[j - 1, 0];
+                    }
+                }
+
+                for(int j = 0; j < array.Length; j++)
+                {
+                    for(int k = 1; k <= n; k++)
+                    {
+                        int pat = 0;
+                        long min = xyps[array[j]][2] * Abs(xyps[array[j]][0]);
+                        if (j > 0)
+                        {
+                            min += dp[j - 1, k];
+                        }
+                        for(int l = j; l >= 0; l--)
+                        {
+                            pat += (1 << (array[l]));
+                            long tmp = horizonBars[pat];
+                            if (l > 0)
+                            {
+                                tmp += dp[l - 1, k - 1];
+                            }
+                            min = Min(min, tmp);
+                        }
+
+                        dp[j, k] = min;
+                    }
+                }
+
+                for (int j = 0; j <= n; j++)
+                {
+                    horizons[i, j] = dp[array.Length - 1, j];
+                }
+            }
+
+            long[] verticalBars = new long[all];
+            for (int i = 0; i < all; i++)
+            {
+                List<int> ys = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i & (1 << j)) > 0)
+                    {
+                        ys.Add(j);
+                    }
+                }
+                int[] array = ys.ToArray();
+                Array.Sort(array, (a, b) => (int)(xyps[a][1] - xyps[b][1]));
+
+                long min = long.MaxValue;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    long tmp = 0;
+                    for (int k = 0; k < array.Length; k++)
+                    {
+                        tmp += xyps[array[k]][2] * Abs(xyps[array[j]][1] - xyps[array[k]][1]);
+                    }
+                    min = Min(min, tmp);
+                }
+
+                verticalBars[i] = min;
+            }
+
+            long[,] verticals = new long[all, n + 1];
+            for (int i = 1; i < all; i++)
+            {
+                List<int> ys = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i & (1 << j)) > 0)
+                    {
+                        ys.Add(j);
+                    }
+                }
+                int[] array = ys.ToArray();
+                Array.Sort(array, (a, b) => (int)(xyps[a][1] - xyps[b][1]));
+
+                long[,] dp = new long[array.Length, n + 1];
+                for (int j = 0; j < array.Length; j++)
+                {
+                    long y = xyps[array[j]][1];
+                    dp[j, 0] = Abs(y) * xyps[array[j]][2];
+                    if (j > 0)
+                    {
+                        dp[j, 0] += dp[j - 1, 0];
+                    }
+                }
+
+                for (int j = 0; j < array.Length; j++)
+                {
+                    for (int k = 1; k <= n; k++)
+                    {
+                        int pat = 0;
+                        long min = xyps[array[j]][2] * Abs(xyps[array[j]][1]);
+                        if (j > 0)
+                        {
+                            min += dp[j - 1, k];
+                        }
+                        for (int l = j; l >= 0; l--)
+                        {
+                            pat += (1 << (array[l]));
+                            long tmp = verticalBars[pat];
+                            if (l > 0)
+                            {
+                                tmp += dp[l - 1, k - 1];
+                            }
+                            min = Min(min, tmp);
+                        }
+
+                        dp[j, k] = min;
+                    }
+                }
+
+                for(int j = 0; j <= n; j++)
+                {
+                    verticals[i, j] = dp[array.Length - 1, j];
+                }
+            }
+
+            for(int i = 0; i <= n; i++)
+            {
+                long res = long.MaxValue;
+                for(int j = 0; j < all; j++)
+                {
+                    for(int k = 0; k <= i; k++)
+                    {
+                        res = Min(res, horizons[j, k] + verticals[all - j - 1, i - k]);
+                    }
+                }
+                WriteLine(res);
             }
         }
 
