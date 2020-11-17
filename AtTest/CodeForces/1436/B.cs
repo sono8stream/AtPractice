403edb56d2866ed5b275f4bc39fba72632f51ce3
SONO <son.stream8580@gmail.com>
2020-11-18 00:16:01 +0900

    CF 1436

---
+++ AtTest/CodeForces/1436/B.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.CodeForces._1436
+{
+    class B
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int t = ReadInt();
+            HashSet<int> primes = PrimeNumbers(100000);
+            for(int i = 0; i < t; i++)
+            {
+                int n = ReadInt();
+                int oneSum = n - 1;
+                int other = int.MaxValue;
+                foreach (int prime in primes) {
+                    if (prime - oneSum > 0 && !primes.Contains(prime - oneSum))
+                    {
+                        other = Min(other, prime - oneSum);
+                    }
+                }
+                int otherSum = other * (n - 1);
+                int other2 = int.MaxValue;
+                foreach(int prime in primes)
+                {
+                    if (prime - otherSum >= 0 && !primes.Contains(prime - otherSum))
+                    {
+                        other2 = Min(other2, prime - otherSum);
+                    }
+                }
+
+                for(int j = 0; j < n; j++)
+                {
+                    for(int k = 0; k < n; k++)
+                    {
+                        if (j == n - 1 && k == n - 1)
+                        {
+                            Write(other2);
+                        }
+                        else if (j == n - 1||k==n-1)
+                        {
+                            Write(other);
+                        }
+                        else
+                        {
+                            Write(1);
+                        }
+
+                        if (k == n - 1)
+                        {
+                            WriteLine();
+                        }
+                        else
+                        {
+                            Write(' ');
+                        }
+                    }
+                }
+            }
+        }
+
+        static HashSet<int> PrimeNumbers(int max)
+        {
+            var primes = new HashSet<int>();
+
+            bool[] isPrime = new bool[max + 1];
+            for(int i = 2; i <= max; i++)
+            {
+                isPrime[i] = true;
+            }
+            for(int i = 2; i <= max; i++)
+            {
+                if (!isPrime[i])
+                {
+                    continue;
+                }
+
+                for(int k = 2; i * k <= max; k++)
+                {
+                    isPrime[i * k] = false;
+                }
+            }
+
+            for (int i = 2; i <= max; i++)
+            {
+                if (isPrime[i])
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtTest/CodeForces/1436/D.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtTest.CodeForces._1436
+{
+    class D
+    {
+        static void ain(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(OpenStandardOutput()) { AutoFlush = false };
+            SetOut(sw);
+
+            Method(args);
+
+            Out.Flush();
+        }
+
+        static void Method(string[] args)
+        {
+            int n = ReadInt();
+            List<int>[] graph = new List<int>[n];
+            for(int i = 0; i < n; i++)
+            {
+                graph[i] = new List<int>();
+            }
+            int[] roots = ReadInts();
+            for (int i = 0; i < n - 1; i++)
+            {
+                int rt = roots[i] - 1;
+                graph[rt].Add(i + 1);
+            }
+            long[] cs = ReadLongs();
+
+            Queue<int> que = new Queue<int>();
+            que.Enqueue(0);
+            Stack<int> stk = new Stack<int>();
+            while (que.Count > 0)
+            {
+                int now = que.Dequeue();
+                stk.Push(now);
+                for(int i = 0; i < graph[now].Count; i++)
+                {
+                    que.Enqueue(graph[now][i]);
+                }
+                if (graph[now].Count == 1)
+                {
+
+                }
+            }
+
+            long[][] dp = new long[n][];
+            while (stk.Count > 0)
+            {
+                int now = stk.Pop();
+
+                if (graph[now].Count == 0)
+                {
+                        dp[now] = new long[3] { cs[now], cs[now], 1 };
+                }
+                else
+                {
+                    long max = 0;
+                    long sum = 0;
+                    long cnt = 0;
+                    for(int i = 0; i < graph[now].Count; i++)
+                    {
+                        int to = graph[now][i];
+                        max = Max(max, dp[to][0]);
+                        sum += dp[to][1];
+                        cnt += dp[to][2];
+                    }
+                    sum += cs[now];
+                    if (sum > max * cnt)
+                    {
+                        max = sum / cnt;
+                        if (sum % cnt > 0)
+                        {
+                            max++;
+                        }
+                    }
+                    dp[now] = new long[3] { max, sum, cnt };
+                }
+            }
+
+            WriteLine(dp[0][0]);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static char[] ReadChars() { return Array.ConvertAll(Read().Split(), a => a[0]); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
